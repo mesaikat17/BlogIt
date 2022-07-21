0c2547b193b4018058968edf6ecdda4acfe8b52d
mesaikat17 <mesaikat17@gmail.com>
2022-07-22 01:51:13 +0530

    Minor Entity Changes

--- src/main/java/com/blog/Entities/Post.java
+++ src/main/java/com/blog/Entities/Post.java
@@ -7,6 +7,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -24,6 +26,45 @@ public class Post {
 	private String headline;
 	private String body;
 	private String category;
+	@DateTimeFormat(pattern = "dd-MM-yyyy hh:mm")
 	private Date date;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getHeadline() {
+		return headline;
+	}
+	public void setHeadline(String headline) {
+		this.headline = headline;
+	}
+	public String getBody() {
+		return body;
+	}
+	public void setBody(String body) {
+		this.body = body;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
 	
 }
