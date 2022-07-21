21d4d9e29847f1a13f2b9d860472d33f838f5573
mesaikat17 <mesaikat17@gmail.com>
2022-07-22 01:50:54 +0530

    Config Changes

--- src/main/java/com/blog/BlogItApplication.java
+++ src/main/java/com/blog/BlogItApplication.java
@@ -1,13 +1,24 @@
 package com.blog;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.blog.Entities.Post;
 
 @SpringBootApplication
+@AutoConfiguration
 public class BlogItApplication {
-
+		
+		@Bean
+		Post post() {
+			return new Post();
+		}
 	public static void main(String[] args) {
 		SpringApplication.run(BlogItApplication.class, args);
+		
+		
 	}
 
 }
